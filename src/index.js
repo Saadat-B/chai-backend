46806ac517714a645e697c8c57adf00a814b524b
saadat-neosoft <saadat.badgujar@neosoftmail.com>
2024-03-30 10:16:28 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
 
 /*
 import express from "express";
